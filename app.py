5f5efe3709a3f18fae15b2e90668dce6881bd878
Manisha Varshney <94631536+manisha-v@users.noreply.github.com>
2024-01-16 23:00:16 +0530

    Exception handling

--- app.py
+++ app.py
@@ -11,17 +11,16 @@ genai.configure(api_key = st.secrets["API_KEY"])
 prompt=[
     """
     You are an expert in converting English questions to SQL query!
-    The SQL database has the name STUDENTS and has the following columns - NAME, CLASS, 
-    Marks, Company \n\nFor example,\nExample 1 - How many entries of records are present?, 
+    The SQL database has the name STUDENTS and has the following columns - NAME, CLASS, Marks, Company 
+    \n\nFor example,\nExample 1 - How many entries of records are present?, 
     the SQL command will be something like this SELECT COUNT(*) FROM STUDENTS ;
     \nExample 2 - Tell me all the students studying in MCA class?, 
-    the SQL command will be something like this SELECT * FROM STUDENT 
-    where CLASS="MCA"; 
+    the SQL command will be something like this SELECT * FROM STUDENTS where CLASS="MCA"; 
     also the sql code should not have ``` in the beginning or end and SQL word in output
     """
 ]
 
-# Load Google Gemini Model and provide sql queries as response
+# Load Google Gemini Model and provide SQL queries as response
 def get_response(que, prompt):
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content([prompt[0], que])
@@ -54,12 +53,14 @@ que = st.text_input("Enter Your SQL Query:", key="sql_query")
 submit = st.button("Get Answer", key="submit_button", help="Click to retrieve the SQL data")
 
 if submit or que:
-    response = get_response(que, prompt)   
-    print(response) 
-    response=read_query(response,"data.db")
-    st.subheader("The Response is:")
-    st.table(response)
-    st.subheader("Enter Valid Text")
+    try:
+        response = get_response(que, prompt)   
+        print(response) 
+        response=read_query(response,"data.db")
+        st.subheader("The Response is:")
+        st.table(response)
+    except:    
+        st.subheader("Enter Valid Text")
 
 st.markdown("""
 ---
